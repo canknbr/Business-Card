b6ca912d9355a1593cfbe68bb1f588189a39b75c
canknbr <cankanbur1@gmail.com>
2023-04-04 22:55:38 +0300

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Can Kanbur on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
