38453caa9618270cb0bd318452ba646d20cd56b9
canknbr <cankanbur1@gmail.com>
2023-04-04 23:16:47 +0300

    added sample

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("bgColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("can").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150).clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(.white, lineWidth: 6)
+                    )
+                Text("Can Kanbur").font(.system(size: 40)).fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("iOS Developer").font(.system(size: 28)).fontWeight(.semibold).foregroundColor(.white)
+                Divider()
+                InfoView(text: "0534 618 0007", iconName: "phone.fill")
+                InfoView(text: "cankanbur1@gmail.com", iconName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    let text: String
+    let iconName : String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25).fill(.white).frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: iconName).foregroundColor(.red)
+                    Text(text)
+                }
+            )
+            .padding(.all)
     }
 }
